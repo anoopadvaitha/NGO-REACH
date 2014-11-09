75395e96f3483185cc902acab0c54ccde60c4b7e
Anoop Chandra <anoop.advaitha@hotmail.com>
2014-11-09 20:37:36 +0530

    NGO REACH

---
+++ PivotApp2/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PivotApp2.Resources;
+
+namespace PivotApp2
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ PivotApp2/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PivotApp2.Resources;
+using Newtonsoft.Json;
+
+namespace PivotApp2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            new_func();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+        public void new_func()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+            webClient.DownloadStringAsync(new Uri("http://ngo.advrdt.com/api/get_posts/"));
+        }
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            var rootObject = JsonConvert.DeserializeObject<NGO>(e.Result.ToString());
+
+
+            NGO.ItemsSource = rootObject.posts;
+ 
+
+
+            foreach (var blog in rootObject.posts)
+            {
+                blog.content = html_tag.StripTagsCharArray(blog.content);
+                if (blog.custom_fields.imgurl != null)
+                {
+                    List<string> logo_rl = blog.custom_fields.imgurl;
+                    blog.logo = logo_rl[0];
+                    //MessageBox.Show(logo_rl[0].ToString());
+                }
+            }
+        }
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
